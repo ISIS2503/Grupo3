c7b0a323a32e7d7fc6b6449bc183ea4fd13c7764
alejandro <alejandro@157.253.93.188>
2017-09-24 19:54:30 +0000

    proyecto minas

---
+++ minas-logic/src/test/java/com/arquisoft/minas/test/persistance/PersistanceTest.java
@@ -0,0 +1,163 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.arquisoft.minas.test.persistance;
+
+import com.arquisoft.minas.entities.SensorEntity;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+/**
+ *
+ * @author jma.lovera10
+ */
+@RunWith(Arquillian.class)
+public class PersistanceTest 
+{
+    
+    /**
+     * Nombre del war
+     */
+    public static final String DEPLOY = "Prueba";
+    
+    /**
+     * 
+     * @return 
+     */
+    @Deployment
+    public static WebArchive createDeployment()
+    {
+        return ShrinkWrap.create(WebArchive.class, DEPLOY + ".war")
+                .addPackage(SensorEntity.class.getPackage())
+                .addPackage(Persistence.class.getPackage())
+                .addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
+                .addAsWebInfResource("META-INF/beans.xml", "beans.xml");
+    }
+    
+    /**
+     * La persistencia de la foto
+     */
+    @Inject
+    private FotoPersistence fotoPersistence;
+    
+    /**
+     * Manejador de entidades
+     */
+    @PersistenceContext
+    private EntityManager em;
+    
+    /**
+     * User transaction
+     */
+    @Inject
+    UserTransaction utx;
+    
+     /**
+     * Configuración inicial de la prueba.
+     *
+     * @generated
+     */
+    @Before
+    public void configTest() 
+    {
+        try {
+            utx.begin();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (IllegalStateException | SecurityException | HeuristicMixedException | HeuristicRollbackException | NotSupportedException | RollbackException | SystemException e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (IllegalStateException | SecurityException | SystemException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+    
+    /**
+     * Limpia las tablas que están implicadas en la prueba.
+     *
+     * @generated
+     */
+    private void clearData() 
+    {
+        em.createQuery("delete from FotoEntity").executeUpdate();
+        em.createQuery("delete from VisitaEntity").executeUpdate();
+    }
+    
+    /**
+     * @generated
+     */
+    private List<FotoEntity> data = new ArrayList<FotoEntity>();
+    
+    /**
+     * @generated
+     */
+    private VisitaEntity visit = null;
+    
+    /**
+     * Inserta los datos iniciales para el correcto funcionamiento de las
+     * pruebas.
+     *
+     * @generated
+     */
+    private void insertData() 
+    {
+        PodamFactory fac = new PodamFactoryImpl();
+        VisitaEntity vis = fac.manufacturePojo(VisitaEntity.class);
+        em.persist(vis);
+        visit = vis;
+        for (int i = 0; i < 3; i++) {
+            PodamFactory factory = new PodamFactoryImpl();
+            FotoEntity entity = factory.manufacturePojo(FotoEntity.class);
+            entity.setVisita(visit);
+            em.persist(entity);
+            data.add(entity);
+        }
+        
+    }
+    
+    /**
+     * Prueba para crear una Foto.
+     *
+     * @generated
+     */
+    @Test
+    public void createFotoTest() 
+    {
+        PodamFactory factory = new PodamFactoryImpl();
+        FotoEntity newEntity = factory.manufacturePojo(FotoEntity.class);
+        newEntity.setVisita(visit);
+        FotoEntity result = fotoPersistence.create(newEntity);
+
+        Assert.assertNotNull("El resultado no puede ser nulo",result);
+
+        FotoEntity entity = em.find(FotoEntity.class, result.getId());
+
+        Assert.assertEquals("El formato de la foto no coincide",newEntity.getFormato(), entity.getFormato());
+        Assert.assertEquals("Los bytes de la imagen no coinciden",new String(newEntity.getValor()), new String(entity.getValor()));
+        Assert.assertEquals("La referencia a la visita no coincide",newEntity.getVisita().getId(), entity.getVisita().getId());
+    }
+}
